11ebe55df220146463b391636accbbd79d7d36e0
Blairren <blairren@tencent.com>
2024-05-21 16:45:35 +0800

    Initial Commit

---
+++ BlairLearnOpenGL/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  BlairLearnOpenGL
+//
+//  Created by yiwen ren on 2024/5/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
