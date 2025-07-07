c4350fd4f13c381a909edd4399a8fa0eab882cd1
Blairren <blairren@tencent.com>
2025-07-07 15:39:05 +0800

    Fix texture load path

--- BlairLearnOpenGL/5_PhongLight.cpp
+++ BlairLearnOpenGL/5_PhongLight.cpp
@@ -111,7 +111,7 @@ int phongLight(){
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    unsigned char* data = stbi_load("../Resource/container2.png", &width, &height, &nrChannels, 0);
+    unsigned char* data = stbi_load("Resource/container2.png", &width, &height, &nrChannels, 0);
     if(data){
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
     }else{
